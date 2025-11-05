ce967ef7297841d6aee122d32b85f66bcdfeb4fd
raihan070418-ai <raihan070418@gmail.com>
2025-11-05 20:25:31 +0700

    Auto-sync: file changes

---
+++ script.js
@@ -0,0 +1,10 @@
+let nilai = 70;
+if (nilai >= 70)
+    console.log("mendapat nilai A");
+
+else if (nilai >= 70)
+    console.log("B");
+else if (nilai >= 60)
+    console.log("C");
+else if (nilai >= 50)
+    console.log("D");   
